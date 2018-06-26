eebc8b81c06cb58403aef372f4fa90ff58b8e297
Eric Castoldi <eric.castoldi@senior.com.br>
2018-06-25 23:52:14 -0300

    GCloud + Spotify

--- config.js
+++ config.js
@@ -1,7 +1,29 @@
 const clientInfo = require('./client_id.json').web;
+const { config } = require('./package.json');
 const { client_id, client_secret } = clientInfo;
 
+const environment = process.env.NODE_ENV && process.env.NODE_ENV === 'production' 
+  ? config.prod
+  : config.dev;
+
+console.log(environment);
+
+const {
+  oauth2callback,
+  MEMCACHE_URL,
+  MEMCACHE_USERNAME,
+  MEMCACHE_PASSWORD,
+  spotifyClientId,
+  spotifyClientSecret
+} = environment;
+
 module.exports = {
+  oauth2callback, 
+  MEMCACHE_URL, 
+  MEMCACHE_USERNAME, 
+  MEMCACHE_PASSWORD,
   consumer_key: client_id,
-  consumer_secret: client_secret
+  consumer_secret: client_secret,
+  spotifyClientId,
+  spotifyClientSecret
 };
--- index.js
+++ index.js
@@ -1,26 +1,61 @@
-const util = require('util');
+
+'use strict';
+
+if (process.env.NODE_ENV === 'production') {
+  require('@google-cloud/trace-agent').start();
+  require('@google-cloud/debug-agent').start();
+}
+
 const express  = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MemcachedStore = require('connect-memjs')(session);
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const gcal = require('google-calendar');
-const config = require('./config');
-const Promise = require("bluebird");
-const { getEvents, getEventsAsync, getCalendarsAsync } = require('./service/calendar');
+const logging = require('./logging');
+const SpotifyWebApi = require('spotify-web-api-node');
+const { getEvents, getCalendars } = require('./service/calendar');
 const app = express();
+
+const {
+  oauth2callback,
+  MEMCACHE_URL,
+  MEMCACHE_USERNAME,
+  MEMCACHE_PASSWORD,
+  consumer_key,
+  consumer_secret,
+  spotifyClientId,
+  spotifyClientSecret
+} = require('./config');
+
+
+const sessionConfig = {
+  resave: false,
+  saveUninitialized: false,
+  secret: 'n3nn5',
+  signed: true
+};
+
+if (MEMCACHE_URL) {
+  sessionConfig.store = new MemcachedStore({
+    servers: [ MEMCACHE_URL ],
+    username: MEMCACHE_USERNAME,
+    password: MEMCACHE_PASSWORD
+  });
+}
+
+app.use(logging.requestLogger);
 
 app.use(cookieParser());
 app.use(bodyParser());
-app.use(session({ secret: 'n3nn5' }));
+app.use(session(sessionConfig));
 app.use(passport.initialize());
 
-
 passport.use(new GoogleStrategy({
-    clientID: config.consumer_key,
-    clientSecret: config.consumer_secret,
-    callbackURL: "http://localhost:8282/oauth2callback",
+    clientID: consumer_key,
+    clientSecret: consumer_secret,
+    callbackURL: oauth2callback,
     scope: ['openid', 'email', 'https://www.googleapis.com/auth/calendar'] 
   },
   function(accessToken, refreshToken, profile, done) {
@@ -41,10 +76,63 @@ app.get('/oauth2callback',
 
 const handleError = (err, res) => {
   const { message } = err;
+  console.log('err', err);
   res.status(400).send({ message });
 };
 
-app.get('/:calendarName', (req, res) => {
+
+const spotifyCredentials = {
+  clientId: spotifyClientId,
+  clientSecret: spotifyClientSecret
+};
+
+const spotifyApi = new SpotifyWebApi(spotifyCredentials);
+
+function spotifyAuth(req, res, next) {
+  if(req.session.spotify_access_token && spotifyApi.getAccessToken()) {
+    return next();
+  }
+
+  const handle = (err) => next(err);
+  spotifyApi.clientCredentialsGrant()
+    .then((data) => {
+
+      const token = data.body['access_token'];
+
+      spotifyApi.setAccessToken(token);
+      req.session.spotify_access_token = token;
+
+      next();
+
+    }, handle)
+    .catch(handle);
+}
+
+app.get('/api/spotify/search', spotifyAuth,(req, res) => {
+
+  const handle = (err) => handleError(err, res);
+  console.log('req.query', req.query)
+  
+  spotifyApi.searchTracks(req.query.q).then(
+    (data) => {
+      console.log(data.body);
+
+      const tracks = data.body.tracks.items;
+      const info = tracks.map(track => {
+        const { name, album } = track;
+        const artist = track.artists.map(artist => artist.name).join(', ');
+
+        return { name, artist, album: album.name };
+      });
+      
+      res.json(info);
+      
+      // res.json(data.body.tracks.items);
+    }, handle).catch(handle);
+});
+
+
+app.get('/api/calendar/:calendarName', (req, res) => {
   if(!req.session.access_token) {
     return res.redirect('/auth');
   }
@@ -60,7 +148,7 @@ app.get('/:calendarName', (req, res) => {
     .catch(handle);
 });
 
-app.all('/', function(req, res){
+app.all('/api/calendar', function(req, res){
   
   if(!req.session.access_token) return res.redirect('/auth');
   
@@ -74,5 +162,19 @@ app.all('/', function(req, res){
     .catch(handle);
 });
 
+app.use(logging.errorLogger);
 
-app.listen(8282);
+// Basic 404 handler
+app.use((req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Basic error handler
+app.use((err, req, res, next) => {
+  /* jshint unused:false */
+  // If our routes specified a specific response, then send that. Otherwise,
+  // send a generic message so as not to leak anything.
+  res.status(500).send(err.response || 'Something broke!');
+});
+
+app.listen(8080);
